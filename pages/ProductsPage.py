d61bdaa14de8da7d08a7397c9fc5cfdf6a8fec7d
Sergei Ignatenko <s.ignatenko.2999@gmail.com>
2023-08-10 19:32:02 +0200

    Provided improvements to align with code standards.

--- pages/ProductsPage.py
+++ pages/ProductsPage.py
@@ -2,10 +2,12 @@ from playwright.sync_api import expect
 
 
 class ProductsPage:
-    """ The class is created in order to have the related methods of the main page
-    to work with products. """
+    """ The class is created in order to have the related methods
+    of the main page to work with products. """
 
     all_items_list = []
+    missing_product_err_msg = ("The provided product "
+                               "is not present on the actual items list.")
 
     def __init__(self, page):
         self.page = page
@@ -38,25 +40,28 @@ class ProductsPage:
                 "button_id": item_button_id,
             })
 
-    def _verify_product_is_present_on_page(self, *args: str) -> None:
+    @staticmethod
+    def _verify_product_is_present_on_page(*args: str) -> None:
         """ Assert a product that should be visible on the page. """
 
         for item in list(args):
             expect(item).to_be_visible()
 
     def select_a_product(self, product_name: str) -> None:
-        """ Select a product if it is present on the main page, otherwise raise an exception. """
+        """ Select a product if it is present on the main page,
+        otherwise raise an exception. """
 
         if self._verify_the_product_is_in_list(product_name):
             button_id = self._get_product_id(product_name)
             button_id_loc = self.page.locator(f"#{button_id}")
             button_id_loc.click()
         else:
-            raise NameError("The provided product is not present on the actual items list.")
+            raise NameError(self.missing_product_err_msg)
 
     def _verify_the_product_is_in_list(self, product_name: str) -> bool:
-        """ Return bool value indicating whether the provided product is present on the main page by looping
-        through the retrieved data from the main page.
+        """ Return bool value indicating whether the provided product is
+        present on the main page by looping through the retrieved data
+        from the main page.
 
         Note: the product name is taken from the json file as test data. """
 
@@ -66,8 +71,8 @@ class ProductsPage:
         return False
 
     def _get_product_id(self, product_name: str) -> str:
-        """ Return button id locator by looping through the retrieved data from the main page
-        of a requested product. """
+        """ Return button id locator by looping through the retrieved data from
+        the main page of a requested product. """
 
         for item in self.all_items_list:
             if item["item_name"] == product_name:
@@ -76,8 +81,12 @@ class ProductsPage:
     def proceed_to_the_cart(self) -> None:
         self.shopping_cart.click()
 
-    def item_count_on_shopping_cart_badge_assertion(self, products_amount: int) -> None:
-        """ Assert whether the amount of products is displayed in accordance with the added amount. """
+    def item_count_on_shopping_cart_badge_assertion(
+            self, products_amount: int
+    ) -> None:
+        """ Assert whether the amount of products is displayed in accordance
+         with the added amount. """
+
         expect(self.shopping_cart_badge).to_be_visible()
         expect(self.shopping_cart_badge).to_have_text(str(products_amount))
 
@@ -88,5 +97,6 @@ class ProductsPage:
 
     # TODO checkout related, to refactor
     def get_list_of_all_added_products(self):
-        # TODO: in case there is more than one item in the cart, retrieve list with all of them
+        # TODO: in case there is more than one item in the cart,
+        #  retrieve list with all of them
         pass
